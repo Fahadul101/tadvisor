5b95ae54f30edba1c29e6ab8a80ebdea5972eac8
Fahadul Islam <fahadulislam.user@outlook.com>
2022-01-06 01:42:44 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography} from '@material-ui/core';
+
+
+import useStyles from './styles.js';
+
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor from green travel.
+        </Typography>
+        {/* <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          </Autocomplete>
+        </Box> */}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
